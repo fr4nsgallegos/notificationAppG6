f6fef266e0f70d9aac35e30736f0921d43f6b68e
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-09 22:39:21 -0500

    export pdf init

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:syncfusion_flutter_xlsio/xlsio.dart' as excel;
 import 'package:logger/logger.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class HomePage extends StatefulWidget {
   @override
@@ -13,6 +16,30 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Logger logger = Logger();
+
+  Future<void> _exportPDF() async {
+    final pdf = pw.Document();
+    pdf.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Container(
+            width: 50,
+            height: 50,
+            color: PdfColor.fromHex("#181818"),
+          );
+        },
+      ),
+    );
+
+    Uint8List bytes = await pdf.save();
+    print(bytes);
+    Directory directory = await getApplicationSupportDirectory();
+    File pdfFile = File("${directory.path}/reportePdf.pdf");
+    await pdfFile.writeAsBytes(bytes, flush: true);
+
+    OpenFile.open("${directory.path}/reportePdf.pdf");
+  }
 
   Future<void> _createExcel() async {
     DateTime now = DateTime.now();
@@ -26,8 +53,8 @@ class _HomePageState extends State<HomePage> {
 
     final excel.Worksheet sheet = workbook.worksheets[0];
 
-    sheet.getRangeByIndex(1, 1).setText("HOLA MUNDO 1");
-    sheet.getRangeByName("A2").setText("HOLA MUNDO 2");
+    sheet.getRangeByIndex(1, 1).setText("TITULO");
+    sheet.getRangeByName("B1").setText("SUBTITULO");
 
     //GUARDAR DOCUMENTO
     final List<int> bytes = workbook.saveAsStream();
@@ -59,6 +86,12 @@ class _HomePageState extends State<HomePage> {
                   _createExcel();
                 },
                 child: Text("Generar excel"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _exportPDF();
+                },
+                child: Text("Generar PDF"),
               ),
             ],
           ),
